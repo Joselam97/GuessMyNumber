d7d8ba0ecfe666fdd2920499e14fb4bd319f5713
Jose Alvarez <jlam97@hotmail.es>
2024-11-06 21:03:55 -0600

    Implementando logica con JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+const randomNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector('.number').textContent = randomNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = "🚫No Number!"
+    } else if (guess == randomNumber){
+        document.querySelector('.message').textContent = '🎉Correct Number!'
+    }
+});
