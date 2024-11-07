20796a724b07d7cec314cb3853f251329da65af5
Jose Alvarez <jlam97@hotmail.es>
2024-11-06 22:06:37 -0600

    Implementa logica al ganar y perder, ademas de estilos en JS

--- script.js
+++ script.js
@@ -3,17 +3,58 @@
 const randomNumber = Math.trunc(Math.random()*20) + 1;
 document.querySelector('.number').textContent = randomNumber;
 
+let score = 10;
+
+//Manipula el boton "Check" y el numero que ingresamos lo pasa a Integer
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
+    //No hay input
     if(!guess){
         document.querySelector('.message').textContent = "🚫No Number!";
-    } else if (guess == randomNumber){
+
+    //Jugador Gana
+    } else if (guess === randomNumber){
         document.querySelector('.message').textContent = '🎉Correct Number!';
+
+        //Cambia el color de fondo a verde cuando se gana
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        //Expande el cuadro blanco cuando se gana
+        document.querySelector('.number').style.width = '30rem';
+
+
+    //Numero mas alto
     } else if (guess > randomNumber){
-        document.querySelector('.message').textContent = "📈Too High!";
+
+        //Va reduciendo score
+        if(score > 0){
+            document.querySelector('.message').textContent = "📈Too High!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        //Score llega a 0
+        } else {
+            document.querySelector('.message').textContent = "💀Game Over!";
+            document.querySelector('.score').textContent = 0;
+            //Cambia el color de fondo a rojo cuando se gana
+            document.querySelector('body').style.backgroundColor = '#ff4d4d';
+        }
+
+    //Numero mas bajo
     } else if(guess < randomNumber){
-        document.querySelector('.message').textContent = "📉Too Low!";
+
+        //Va reduciendo score
+        if(score > 0){
+            document.querySelector('.message').textContent = "📉Too Low!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        //Score llega a 0
+        } else {
+            document.querySelector('.message').textContent = "💀Game Over!";
+            document.querySelector('.score').textContent = 0;
+            //Cambia el color de fondo a rojo cuando se gana
+            document.querySelector('body').style.backgroundColor = '#ff4d4d';
+        }
     }
 });
